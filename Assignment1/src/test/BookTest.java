6bc107b0a65d2c7b54f655ad04bdc5695b5cdf96
Stephane_KI <ki.stephane.dambi@gmail.com>
2014-11-06 16:50:59 +0200

    Init test class and requirement file

---
+++ Assignment1/src/test/BookTest.java
@@ -0,0 +1,69 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBookDoubleDoubleDoubleBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBasePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBasePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetVat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDiscount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDiscount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBestSeller() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBestSeller() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSellPrice() {
+		fail("Not yet implemented");
+	}
+
+}
