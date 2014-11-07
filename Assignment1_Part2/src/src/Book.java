6ef9cdd683673f6d858c17a3119c793e36a8c6e8
Stephane_KI <ki.stephane.dambi@gmail.com>
2014-11-07 15:09:34 +0200

    Creation of the second part

---
+++ Assignment1_Part2/src/src/Book.java
@@ -0,0 +1,118 @@
+package src;
+public class Book {
+	
+	public static final double DEFAULT_BASE_PRICE = 0.01;
+	
+	private double basePrice = 0;
+	private double vat = 0;
+	private double discount = 0;
+	private double sellPrice;
+	private boolean bestseller = false;
+	
+
+	public Book() {
+		this.basePrice = 0;
+		this.vat = 0;
+		this.discount = 0;
+		this.bestseller = false;
+	}
+
+	public Book(double basePrice, double discount, double vat, boolean bestseller) {
+		if(basePrice<0){
+			this.basePrice=0.0;
+		}else{
+			this.basePrice = basePrice;
+		}
+		if (discount<0){
+			this.discount=0;
+		}else{
+			this.discount = discount;
+		}		
+		if(vat<0){
+			this.vat=0;
+		}else{
+			this.vat = vat;
+		}
+		
+		this.bestseller = bestseller;
+		setSellPrice();
+	}
+	
+
+	public double getBasePrice() {
+		return basePrice;
+	}
+
+	public void setBasePrice(double basePrice) {
+		if(basePrice<0.0){
+			System.out.println("Base price should not be negative");
+			this.basePrice=0.0;
+		}else if (basePrice==0.0){
+			throw new IllegalArgumentException("Base price should not be zero");
+		}else{		
+			this.basePrice = basePrice;
+		}		
+		setSellPrice();
+	}
+
+	public double getVat() {
+		return vat;
+	}
+
+	public void setVat(double vat) {
+		if (vat<0){
+			this.vat=0.0;
+		}else{
+			this.vat = vat;
+		}
+		setSellPrice();
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		if (discount > 50){
+			System.out.println("discount can not be larger than 50 %");
+			this.discount = 50;
+		}
+		else if(discount<0){
+			System.out.println("discount can not be negative");
+			this.discount = 0;
+		}
+		else {
+			this.discount = discount;
+		}
+		setSellPrice();
+	}
+	
+	public boolean getBestSeller(){
+		return bestseller; 
+	}
+	
+	public void setBestSeller(){
+		this.bestseller = true;
+		setSellPrice();
+	}
+
+	public double getSellPrice() {
+		return sellPrice;
+	}
+
+	private void setSellPrice() {
+		if(basePrice > 0){
+			if(bestseller == true){
+				sellPrice = basePrice *((100 + (vat - discount* 0.5)) / 100);
+			}
+			else{
+			sellPrice = basePrice * ((100 + (vat - discount)) / 100);
+			}
+		}
+		else
+		{
+			System.out.println("The base price must be greater than zero"); 
+		}
+	}
+
+}
---
+++ Assignment1_Part2/src/test/BookTest.java
@@ -0,0 +1,159 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import src.Book;
+
+public class BookTest {
+	public Book book;
+	@Before
+	public void setUp() throws Exception {
+		book= new Book();
+	}
+
+	@Test
+	public void testBasePriceShouldBeSettable() {
+		book.setBasePrice(50.49);
+		assertEquals(50.49, book.getBasePrice(), 0.01);
+	}
+	
+	@Test
+	public void testBasePriceShouldBePositive() {
+		book.setBasePrice(-1);
+		assertEquals(0.0, this.book.getBasePrice(), 0);
+	}
+	
+	@Test
+	public void testOnCreateBasePriceShouldBeGreaterThanZero() {
+		try {
+			this.book= new Book(-1.0, 0.0, 0.0, false);
+		} catch (IllegalArgumentException e) {
+			assertEquals("Base price should be greater than 0", e.getMessage());
+		}
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testBasePriceShouldNotBeSetToZero() {
+		book.setBasePrice(0);
+	}
+	
+	@Test
+	public void testVATshouldBeSettable() {
+		double expected = 0.1;
+		this.book.setVat(expected);
+		assertEquals(expected, this.book.getVat(), 0.0);
+	}
+	
+	@Test
+	public void testVATshouldBeGreaterOrEqualToZero() {
+		this.book.setVat(-0.1);
+		assertEquals(0, this.book.getVat(), 0);
+		
+	}
+	
+	@Test
+	public void testDiscountShouldBeSettable() {
+		double expected = 0.1;
+		this.book.setDiscount(expected);
+		assertEquals(expected, this.book.getDiscount(), 0.0);
+	}
+	
+	@Test
+	public void testDiscountShouldBeGreaterOrEqualToZero() {
+		this.book.setDiscount(-1);
+		assertEquals(0, this.book.getDiscount(), 0);
+	}
+	
+	@Test
+	public void testDiscountShouldBeLessOrEqualThanFiftyPercent() {
+		this.book.setDiscount(50.01);
+		assertEquals(50, this.book.getDiscount(), 0);
+	}
+	
+	@Test
+	public void testSystemShouldCalculateSellPriceAfterAnyVariableChange() {
+		double expectedSellPrice=10;
+		this.book.setBasePrice(10);
+		assertEquals(expectedSellPrice, this.book.getSellPrice(), 0);
+		
+		this.book.setDiscount(50);
+		expectedSellPrice=5.0;
+		assertEquals(expectedSellPrice, this.book.getSellPrice(),0);
+		
+		this.book.setVat(10);
+		expectedSellPrice = 6.0;
+		assertEquals(expectedSellPrice, this.book.getSellPrice(),0);
+		
+		this.book.setBestSeller();
+		expectedSellPrice = 8.5;
+		assertEquals(expectedSellPrice, this.book.getSellPrice(),0);		
+	}
+	
+	@Test
+	public void testOnCreateSellPriceShouldBeCalculated() {
+		double expected= 8.5;
+		this.book = new Book(10,50,10,true);
+		assertEquals(expected, this.book.getSellPrice(), 0);
+	}
+	
+	@Test
+	public void testBestSellerRealDiscountIsFiftyPercentOfActualDiscount() {
+		double expected= 6.0;
+		this.book = new Book(10,50,10,false);
+		assertEquals(expected, this.book.getSellPrice(), 0);
+		
+		this.book.setBestSeller();
+		expected=8.5;
+		assertEquals(expected, this.book.getSellPrice(), 0);
+	}
+	
+	@Test
+	public void testBestSellerAttributShouldBeSettable() {
+		assertFalse(this.book.getBestSeller());
+		this.book.setBestSeller();
+		assertTrue(this.book.getBestSeller());
+	}
+	
+	@Test
+	public void testBasePriceShouldBeSetToZeroIfUserSetItNegative() {
+		this.book.setBasePrice(-1.0);
+		assertEquals(0.0, this.book.getBasePrice(), 0);
+	}
+	
+	@Test
+	public void testOnCreateBasePriceShouldBeSetToZeroIfNegative() {
+		this.book = new Book(-10,10,10,false);
+		assertEquals(0.0, this.book.getBasePrice(), 0);
+	}
+	
+	@Test
+	public void testVATsetToZeroIfUserTryNegativeValue() {
+		this.book.setVat(-10);
+		assertEquals(0.0, this.book.getVat(), 0);
+	}
+	
+	@Test
+	public void testOnCreateVATSetToZeroIfUserEnterNegativeValue() {
+		this.book = new Book(10,10,-10,false);
+		assertEquals(0.0, this.book.getVat(), 0);
+	}
+	
+	@Test
+	public void testDiscountsetToZeroIfUserTryNegativeValue() {
+		this.book.setDiscount(-10);
+		assertEquals(0.0, this.book.getDiscount(), 0);
+	}
+	
+	@Test
+	public void testOnCreateDiscountSetToZeroIfUserEnterNegativeValue() {
+		this.book = new Book(10,-10,10,false);
+		assertEquals(0.0, this.book.getDiscount(), 0);
+	}
+	
+	
+
+
+
+}
